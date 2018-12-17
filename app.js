d8851d1f9727ce108d979d804acb85e6e540edd5
unknown <w.andrew.pedersen@gmail.com>
2018-12-17 14:58:26 -0700

    Express sit up and working

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// HERE WE GO ! 
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 4444
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+app.listen(port, () => console.log(`We are balling hard Port:${port}`))
+
+app.use(cors())
+app.use(bodyParser.json())
