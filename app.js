58f8c3a03f792f437f3df9397a164baa562b269c
unknown <w.andrew.pedersen@gmail.com>
2018-12-18 11:29:34 -0700

    Fixed routing issues

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express()
 const port = process.env.PORT || 4444
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const queries_users_a = require("./queries_users_a")
-
-app.listen(port, () => console.log(`We are balling hard Port:${port}`))
-
-app.get("/", (req,res) => {
-   queries_users_a.listAll().then(users_a => res.json(users_a))
-})
 
 app.use(cors())
 app.use(bodyParser.json())
+
+
+
+const users_a = require("./users_a")
+
+app.use("/users_a", users_a)
+
+app.listen(port, () => console.log(`We are balling hard Port:${port}`))
+
+// app.get("/", (req,res) => {
+//    queries_users_a.listAll().then(users_a => res.json(users_a))
+// })
+
--- queries_users_a.js
+++ queries_users_a.js
@@ -3,7 +3,25 @@ const db = require("./database-connection")
 module.exports = {
     
     listAll() {
-        return db('users_a')
-    }
-
+        return db("users_a")
+    },
+    createAdopteeUser(createAdoptee) {
+        return db("users_a")
+        .insert(createAdoptee)
+        .returning("*")
+    },
+    deleteAdopteeUser(id) {
+        return db("users_a")
+        .where("id",id)
+        .delete()
+    },
+    getById(id) {
+        return db("users_a").where("id", id)
+    },
+    updateAdopteeUser(id, adoptee) {
+        return db("users_a")
+          .where('id', id)
+          .update(adoptee)
+          .returning('*')
+      }
 }
