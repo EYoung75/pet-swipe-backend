848bfde533c5a11b501f75c7d440eae21f16853b
unknown <w.andrew.pedersen@gmail.com>
2018-12-18 14:19:03 -0700

    fix those errors

--- database-connection.js
+++ database-connection.js
@@ -1,4 +1,4 @@
-const enviroment = process.env.NODE_EN || "development"
+const enviroment = process.env.NODE_ENV || "development"
 const config = require("./knexfile.js")
 const configEnv = config[enviroment]
 const knex = require("knex")
