a2586475765c92d8af82e7928978a32d412b0537
EYoung75 <eyoung75.ey@gmail.com>
2019-01-17 15:59:06 -0700

    began implementing auth and downloaded bcrypt

--- migrations/20181217153444_users_a.js
+++ migrations/20181217153444_users_a.js
@@ -2,12 +2,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users_a", (users) => {
     users.increments("id")
-    users.string("user_name_a")
+    users.string("user_name_a").unique()
     users.string("user_name_a_pw")
     users.string("location_city")
     users.string("location_lat")
     users.string("location_long")
-    users.string("contact_email")
+    users.string("contact_email").unique()
     users.string("picture_url", 500)
   }) 
 }
--- users_a.js
+++ users_a.js
@@ -1,23 +1,47 @@
 const express = require("express")
 const router = express.Router()
 const queries = require("./queries_users_a.js")
+const bcrypt = require("bcrypt")
+const app = express()
+const saltRounds = 12 
+
 
 
     
-    router.get("/", (req,res) => {
+    router.get("/adoptee", (req,res) => {
        queries.listAll().then(adoptee => res.status(200).send(adoptee))
     })
     router.get("/:id", (req,res) => {
         queries.getById(req.params.id).then(adoptee => res.status(200).send(adoptee))
     })
     router.post("/", (req, res, next) => {
-        queries.createAdopteeUser(req.body).then(adoptee => res.status(201).send(adoptee))
+        const userNewPassword = brypt.hashSync(req.body.password, saltRounds)
+        console.log(userNewPassword)
+        const newUser = {
+            user_name_a: req.body.user_name_a,
+            user_name_a_pw: userNewPassword,
+            email: req.body.contact_email,
+            location_city: req.body.location_city
+        }
+        queries.createAdopteeUser(newUser).then(user => res.status(201).send(user[0]))
+        .catch(next)
     })
     router.delete("/:id", (req, res) => {
         queries.deleteAdopteeUser(req.params.id).then(res.sendStatus(204))
     })
     router.put("/:id", (req, res) => {
         queries.updateAccounts(req.params.id, req.body).then(adoptee => res.status(200).send(adoptee))
+    })
+
+
+    app.use((req, res) => {
+        res.status(404).json({error: {message: "User not found"}})
+    })
+
+    app.use((err, req, res, next) => {
+        console.log("ERROR", err)
+        const status = err.status || 500
+        res.status(status).json({ error: err.message })
     })
 
 
